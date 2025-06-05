85e26bf4553d2f5bf155a3741c7c6dacfa2b6b9c
Ryan O'Connor <ryan@assemblyai.com>
2025-06-05 12:28:50 -0400

    Update for LiveKit agents v1.x

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Real-time Speech-to-Text in LiveKit with AssemblyAI
 
-*NOTE: This repository uses version 0.x of `livekit-agents`*
-
 This repository contains a minimal example of how to use AssemblyAI's real-time transcription API with LiveKit. See the companion blog [**How to add real-time Speech-to-Text to your LiveKit application**](https://www.assemblyai.com/blog/livekit-realtime-speech-to-text) for a full walkthrough.
 
 ![demo](demo.gif)
@@ -10,11 +8,11 @@ See the companion blog for more detailed instructions on how to set up and run t
 
 1. Clone this repository and `cd` into it
 2. Rename `.env.example` to `.env`
-3. Go to [livekit.io](https://livekit.io) and sign up
+3. Go to [livekit.io](https://livekit.io) and sign up for free
 4. In your LiveKit dashboard, go to `Settings > Keys` and click on your API key
 5. Copy the URL, API key, and secret values into the `.env` file
-6. Go to [assemblyai.com](https://www.assemblyai.com/dashboard/signup?utm_source=blog&utm_medium=internal-link&utm_campaign=livekit-realtime-speech-to-text) and sign up, and then set up billing to get access to [Streaming Speech-to-Text](https://www.assemblyai.com/docs/speech-to-text/streaming?utm_source=blog&utm_medium=internal-link&utm_campaign=livekit-realtime-speech-to-text)
+6. Go to [assemblyai.com](https://www.assemblyai.com/dashboard/signup?utm_source=blog&utm_medium=internal-link&utm_campaign=livekit-realtime-speech-to-text) to sign up for free
 7. Copy your AssemblyAI API key into the `.env` file
-7. `pip install -r requirements.txt` to install requirements
-8. `python stt_agent.py` to run the Speech-to-Text agent
-9. Go the the [LiveKit Agents Playground](https://agents-playground.livekit.io/) and sign in with your LiveKit account. Begin speaking and you will see your speech transcribed in real-time both in the Playground and in the agent's terminal.
+8. `pip install -r requirements.txt` to install requirements
+9. `python stt_agent.py` to run the Speech-to-Text agent
+10. Go the the [LiveKit Agents Playground](https://agents-playground.livekit.io/) and sign in with your LiveKit account. Begin speaking and you will see your speech transcribed in real-time both in the Playground and in the agent's terminal.
--- stt_agent.py
+++ stt_agent.py
@@ -1,15 +1,17 @@
-import asyncio
 import logging
 
 from dotenv import load_dotenv
-from livekit import rtc
+
 from livekit.agents import (
+    Agent,
+    AgentSession,
     AutoSubscribe,
     JobContext,
+    RoomOutputOptions,
+    StopResponse,
     WorkerOptions,
     cli,
-    stt,
-    transcription,
+    llm,
 )
 from livekit.plugins import assemblyai
 
@@ -17,55 +19,37 @@ load_dotenv()
 
 logger = logging.getLogger("transcriber")
 
+class Transcriber(Agent):
+    def __init__(self):
+        super().__init__(
+            instructions="not-needed",
+            stt=assemblyai.STT(),
+        )
+
+    async def on_user_turn_completed(self, chat_ctx: llm.ChatContext, new_message: llm.ChatMessage):
+        # Add any backend processing of transcripts here if needed
+        user_transcript = new_message.text_content
+        logger.info(f" -> {user_transcript}")
+
+        # Needed to stop the agent's default conversational loop
+        raise StopResponse()
+
+
 async def entrypoint(ctx: JobContext):
-    logger.info(f"Starting transcriber (speech to text) example, room: {ctx.room.name}")
-    stt_impl = assemblyai.STT()
-
-    @ctx.room.on("track_subscribed")
-    def on_track_subscribed(
-        track: rtc.Track,
-        publication: rtc.TrackPublication,
-        participant: rtc.RemoteParticipant,
-    ):
-        if track.kind == rtc.TrackKind.KIND_AUDIO:
-            asyncio.create_task(transcribe_track(participant, track))
-
-    async def transcribe_track(participant: rtc.RemoteParticipant, track: rtc.Track):
-        """
-        Handles the parallel tasks of sending audio to the STT service and 
-        forwarding transcriptions back to the app.
-        """
-        audio_stream = rtc.AudioStream(track)
-        stt_forwarder = transcription.STTSegmentsForwarder(
-            room=ctx.room, participant=participant, track=track
-        )
-
-        stt_stream = stt_impl.stream()
-
-        # Run tasks for audio input and transcription output in parallel
-        await asyncio.gather(
-            _handle_audio_input(audio_stream, stt_stream),
-            _handle_transcription_output(stt_stream, stt_forwarder),
-        )
-
-    async def _handle_audio_input(
-        audio_stream: rtc.AudioStream, stt_stream: stt.SpeechStream
-    ):
-        """Pushes audio frames to the speech-to-text stream."""
-        async for ev in audio_stream:
-            stt_stream.push_frame(ev.frame)
-
-    async def _handle_transcription_output(
-        stt_stream: stt.SpeechStream, stt_forwarder: transcription.STTSegmentsForwarder
-    ):
-        """Receives transcription events from the speech-to-text service."""
-        async for ev in stt_stream:
-            if ev.type == stt.SpeechEventType.FINAL_TRANSCRIPT:
-                print(" -> ", ev.alternatives[0].text)
-
-            stt_forwarder.update(ev)
-
+    logger.info(f"starting transcriber (speech to text) example, room: {ctx.room.name}")
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    session = AgentSession()
+
+    await session.start(
+        agent=Transcriber(),
+        room=ctx.room,
+        room_output_options=RoomOutputOptions(
+            # If you don't want to send the transcription back to the room, set this to False
+            transcription_enabled=True,
+            audio_enabled=False,
+        ),
+    )
 
 
 if __name__ == "__main__":
